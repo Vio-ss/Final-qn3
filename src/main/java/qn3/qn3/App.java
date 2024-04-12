23c75185ed35511cfdb8676c0441c2bec2832ac0
Swetha S <swethassivasankaran@gmail.com>
2024-04-12 18:42:18 +0530

    1

---
+++ src/main/java/qn3/qn3/App.java
@@ -0,0 +1,13 @@
+package qn3.qn3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/qn3/qn3/CalculatorApp.java
@@ -0,0 +1,13 @@
+package qn3.qn3;
+
+public class CalculatorApp {
+	public int addMethod(int a, int b) {
+		return(a+b);
+	}
+	public int subMethod(int a, int b) {
+		return(a-b);
+	}
+	public int mulMethod(int a, int b) {
+		return(a*b);
+	}
+}
