1d6fc3dd0be70c331edb078369b2ef5eb3a6d3d8
Swetha S <swethassivasankaran@gmail.com>
2024-04-12 18:58:25 +0530

    4

--- src/test/java/qn3/qn3/CalculatorAppTest.java
+++ src/test/java/qn3/qn3/CalculatorAppTest.java
@@ -6,31 +6,37 @@ import junit.framework.Assert;
 
 public class CalculatorAppTest {
 	@Test
-	@Parameters({"a","b"})
-	public void testAdd(String sa, String sb) {
+//	@Parameters({"a","b"})
+	public void testAdd() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+//		int a = Integer.parseInt(sa);
+//		int b = Integer.parseInt(sb);
+		int a=10;
+		int b=5;
 		int actual=app.addMethod(a, b);
 		int expected=a+b;
 		Assert.assertEquals(actual, expected);
 	}
 	@Test
-	@Parameters({"a","b"})
-	public void testSub(String sa, String sb) {
+//	@Parameters({"a","b"})
+	public void testSub() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+//		int a = Integer.parseInt(sa);
+//		int b = Integer.parseInt(sb);
+		int a=10;
+		int b=5;
 		int actual=app.subMethod(a,b);
 		int expected=a-b;
 		Assert.assertEquals(actual, expected);
 	}
 	@Test
-	@Parameters({"a","b"})
-	public void testMul(String sa, String sb) {
+//	@Parameters({"a","b"})
+	public void testMul() {
 		CalculatorApp app = new CalculatorApp();
-		int a = Integer.parseInt(sa);
-		int b = Integer.parseInt(sb);
+//		int a = Integer.parseInt(sa);
+		int a=10;
+		int b=5;
+//		int b = Integer.parseInt(sb);
 		int actual=app.mulMethod(a, b);
 		int expected=a*b;
 		Assert.assertEquals(actual, expected);
